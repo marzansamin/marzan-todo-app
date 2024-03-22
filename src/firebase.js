6b68f6c5c4590c60bda3b4218b684ecd53b76512
marzansamin <msaminashrafi@gmail.com>
2024-03-22 15:31:51 +0600

    Allah Bachao

--- src/firebase.js
+++ src/firebase.js
@@ -20,23 +20,4 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const fbFunctions = getFunctions(app);
 
-if (process.env.NODE_ENV === "development") {
-  // You can't use process.env in the client-side code
-  // Instead, use a hardcoded value or configure your development environment accordingly
-  const firebaseAuthEmulatorUrl = "http://localhost:9099";
-  const firestoreEmulatorHost = "localhost";
-  const firestoreEmulatorPort = 8080;
-  const functionsEmulatorHost = "localhost";
-  const functionsEmulatorPort = 5001;
-
-  try {
-    connectAuthEmulator(auth, firebaseAuthEmulatorUrl);
-    connectFirestoreEmulator(db, firestoreEmulatorHost, firestoreEmulatorPort);
-    connectFunctionsEmulator(fbFunctions, functionsEmulatorHost, functionsEmulatorPort);
-    console.log("Firebase emulators connected successfully.");
-  } catch (error) {
-    console.error("Error connecting Firebase emulators:", error);
-  }
-}
-
 export { app, db, auth, fbFunctions };
